26628ef6fd7b3dcd7e2fb9e6178db721a2ee4b0f
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-12-22 21:00:03 -0600

    Add turn logic
    The turn is initialized as 1. The game.playTurn() function calls the
    gameBoard.mark() function to attempt to mark a square.
    If the square is occupied, mark returns, and game.playTurn() returns
    before increasing the turn number.
    If the square was filled, mark returns true and then playTurn does
    increase the turn number.

--- script.js
+++ script.js
@@ -4,25 +4,63 @@
   // This should take (player, square)
   // If square is full return
   // Depending on the value of player, use X - O
-
+* Create player factory:
+  * Create id
+  * Add name functionality
+  * 
 */
 
 const gameBoard = (() => {
   const board = [null, null, null, null, null, null, null, null, null];
   const mark = (square, player) => {
     if (board[square] === "X" || board[square] === "O") return;
-    const marker = player.id === "p1" ? "X" : "O";
-    board[square] = marker;
-    // console.log(board);
-    return board;
+    board[square] = player.marker;
+    return true;
   };
+  const printBoard = () => board;
   return {
     mark,
+    printBoard,
   };
 })();
 
-playerOne = {
-  id: "p1",
+const createPlayer = (marker) => {
+  return { marker };
 };
 
-console.log(gameBoard.mark(0, playerOne));
+const game = (() => {
+  let turn = 1;
+  const playTurn = (square) => {
+    const isOdd = (n) => n % 2 !== 0;
+    //* Cross only plays odd turns. Circle only plays even turns.
+    const currentPlayer = isOdd(turn) ? p1 : p2;
+    console.log(currentPlayer);
+    const moveResult = gameBoard.mark(square, currentPlayer);
+    //* Don't increase turn if move wasn't successful (square was occupied)
+    if (!moveResult) return;
+    ++turn;
+  };
+  const printTurn = () => turn;
+
+  return {
+    playTurn,
+    printTurn,
+  };
+})();
+
+p1 = createPlayer("X");
+p2 = createPlayer("O");
+
+console.log(game.printTurn());
+game.playTurn(0);
+console.log(gameBoard.printBoard());
+
+console.log(game.printTurn());
+game.playTurn(1);
+console.log(gameBoard.printBoard());
+
+console.log(game.printTurn());
+game.playTurn(4);
+console.log(gameBoard.printBoard());
+
+console.log(game.printTurn());
