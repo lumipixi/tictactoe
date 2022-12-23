14020d77233c0c6790d3b680094b195ab9390895
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-12-22 23:27:50 -0600

    Check if there is a winner / Print board to console
    Starting on the 5th turn, game.playTurn() will use
    gameBoard.checkWinner() to verify if there is a winner. If it returns
    true then game.playTurn() announces that the current player (and
    therefore the latest player to make a move) won.
    
    game.printBoard() was added to represent the board in the console for
    easy debugging. It prints a string representing the squares. It creates
    a newline for each row.

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-/*
-* Create gameBoard
-* Create function to fill array element:
-  // This should take (player, square)
-  // If square is full return
-  // Depending on the value of player, use X - O
-* Create player factory:
-  * Create id
-  * Add name functionality
-  * 
-*/
-
 const gameBoard = (() => {
-  const board = [null, null, null, null, null, null, null, null, null];
+  const board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
   const mark = (square, player) => {
     if (board[square] === "X" || board[square] === "O") return;
     board[square] = player.marker;
     return true;
   };
-  const printBoard = () => board;
+  const printBoard = () => {
+    let output = "";
+    board.forEach((square, index) => {
+      //*new line for each row
+      if (index === 3 || index === 6) {
+        output += "\n";
+      }
+      output += ` ${square} `;
+    });
+    return output;
+  };
+  const checkWinner = () => {
+    const checkTriplet = (triplet) => {
+      const value = triplet[0];
+      if (!(value === "X" || value === "O")) return undefined;
+      console.log(triplet);
+      return triplet[1] === value && triplet[2] === value ? true : false;
+    };
+    //*Check rows
+    const row1 = checkTriplet(board.slice(0, 3));
+    const row2 = checkTriplet(board.slice(3, 6));
+    const row3 = checkTriplet(board.slice(6));
+    //*Check cols
+    const col1 = checkTriplet([board[0], board[3], board[6]]);
+    const col2 = checkTriplet([board[1], board[4], board[7]]);
+    const col3 = checkTriplet([board[2], board[5], board[8]]);
+    //*Check diagonally
+    const diag1 = checkTriplet([board[0], board[4], board[8]]);
+    const diag2 = checkTriplet([board[6], board[4], board[2]]);
+    //*If one row, col or diag is true, there is a winner
+    return row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2;
+  };
+
   return {
     mark,
     printBoard,
+    checkWinner,
   };
 })();
 
@@ -34,10 +55,13 @@ const game = (() => {
     const isOdd = (n) => n % 2 !== 0;
     //* Cross only plays odd turns. Circle only plays even turns.
     const currentPlayer = isOdd(turn) ? p1 : p2;
-    console.log(currentPlayer);
     const moveResult = gameBoard.mark(square, currentPlayer);
     //* Don't increase turn if move wasn't successful (square was occupied)
     if (!moveResult) return;
+    //* The game can be won starting from the 5th turn.
+    if (turn >= 5 && gameBoard.checkWinner()) {
+      return `${currentPlayer.marker} wins!`;
+    }
     ++turn;
   };
   const printTurn = () => turn;
@@ -52,11 +76,11 @@ p1 = createPlayer("X");
 p2 = createPlayer("O");
 
 console.log(game.printTurn());
-game.playTurn(0);
+game.playTurn(6);
 console.log(gameBoard.printBoard());
 
 console.log(game.printTurn());
-game.playTurn(1);
+game.playTurn(5);
 console.log(gameBoard.printBoard());
 
 console.log(game.printTurn());
@@ -64,3 +88,9 @@ game.playTurn(4);
 console.log(gameBoard.printBoard());
 
 console.log(game.printTurn());
+game.playTurn(7);
+console.log(gameBoard.printBoard());
+
+console.log(game.printTurn());
+console.log(game.playTurn(2));
+console.log(gameBoard.printBoard());
